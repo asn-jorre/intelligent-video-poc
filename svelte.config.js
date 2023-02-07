43e180f2506d959a3c0032470ed27ba132182ac6
Jorre Heuts <heuts.jorre@gmail.com>
2023-02-07 14:21:52 +0100

    add svelte static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
+	//preprocess: preprocessor(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// fallback: '200.html',
+			fallback: null,
+			pages: 'build-static',
+			assets: 'build-static',
+			precompress: false,
+			trailingSlash: 'always'
+		})
 	}
 };
-
-export default config;
